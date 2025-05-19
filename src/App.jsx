0e6cbf30a7bd96eb1da97c87dfde7ccebcb29107
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-05-20 00:05:34 +1000

    Add nanoid for unique ID generation in todo creation and update form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { useState } from "react";
 
 const App = () => {
@@ -6,38 +7,28 @@ const App = () => {
   ]);
 
   const [title, settitle] = useState("");
-  const [completed, setcompleted] = useState(false)
-  const [gender, setgender] = useState("male")
-  const [city, setcity] = useState("mumbai")
 
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    const newtodo = {
+      id: nanoid(),
+      title,
+      isCompleted: false,
+    };
+
+    console.log(newtodo);
+
+  }
   return (
     <>
       <h1>Create Tasks</h1>
-      <form>
+      <form onSubmit={SubmitHandler}>
         <input
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => settitle(e.target.value)}
           value={title}
           type="text"
           placeholder="title"
         />
-        <br />
-        <input
-          checked={completed}
-          onChange={(e) => setcompleted(e.target.checked)}
-          type="checkbox"
-        />{" "}
-        Completed
-        <br /><br />
-        <input value="Male" onChange={(e) => setgender(e.target.value)} checked={gender == "male" && true} type="radio" />
-        male
-        <input value="Female" onChange={(e) => setgender(e.target.value)} checked={gender == "Female" && true} type="radio" />
-        Female
-        <br />
-        <select value={city} onChange={(e) => setcity(e.target.value)}>
-          <option value="delhi">Delhi</option>
-          <option value="mumbai">Mumbai</option>
-          <option value="kolkata">Kolkata</option>
-        </select>
         <br />
         <br />
         <button>Create Todo</button>
