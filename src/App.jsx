47ff0e59b23389153a0f467f7049d624e64e4db4
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-05-19 19:01:06 +0530

    Enhance App component by adding state management for completed status, gender, and city selection in the todo form ( two way binding)

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,38 @@ const App = () => {
   ]);
 
   const [title, settitle] = useState("");
+  const [completed, setcompleted] = useState(false)
+  const [gender, setgender] = useState("male")
+  const [city, setcity] = useState("mumbai")
 
   return (
     <>
       <h1>Create Tasks</h1>
       <form>
         <input
-          onChange={(e) => settitle(e.target.value)}
+          onChange={(e) => console.log(e.target.value)}
           value={title}
           type="text"
           placeholder="title"
         />
         <br />
-        <input type="checkbox" /> Completed
+        <input
+          checked={completed}
+          onChange={(e) => setcompleted(e.target.checked)}
+          type="checkbox"
+        />{" "}
+        Completed
+        <br /><br />
+        <input value="Male" onChange={(e) => setgender(e.target.value)} checked={gender == "male" && true} type="radio" />
+        male
+        <input value="Female" onChange={(e) => setgender(e.target.value)} checked={gender == "Female" && true} type="radio" />
+        Female
+        <br />
+        <select value={city} onChange={(e) => setcity(e.target.value)}>
+          <option value="delhi">Delhi</option>
+          <option value="mumbai">Mumbai</option>
+          <option value="kolkata">Kolkata</option>
+        </select>
         <br />
         <br />
         <button>Create Todo</button>
